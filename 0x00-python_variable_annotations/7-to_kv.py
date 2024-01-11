17401a1fe8a297025d9dc71be1ce3116e786818e
joelsteven <joelofelectronics@gmail.com>
2024-01-11 22:03:55 +0300

    Add type-annotated function to_kv

---
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Type-annotated function to_kv"""
+from typing import Union, Tuple
+
+
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    """Takes str k and an int OR float v as arguments and returns a tuple"""
+    return (k, (v ** 2))

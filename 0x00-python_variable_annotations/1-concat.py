2cb527891da08b63ecb770e2bbd92ef8755e5b25
joelsteven <joelofelectronics@gmail.com>
2024-01-11 21:09:08 +0300

    Fixed concat function pycodestyle

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """Type-annotated function concat"""
 
+
 def concat(str1: str, str2: str) -> str:
-    """Takes string str1 and str2 as arguments and returns concatenated string"""
+    """Takes str str1 and str2 as args and returns concatenated string"""
     return str1 + str2

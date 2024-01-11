d3a8c401b8e5aa63cb847463ef3db651c4a0214f
joelsteven <joelofelectronics@gmail.com>
2024-01-11 20:54:50 +0300

    Added repo config files and task_0

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Type-annotated function add"""
+
+
+def add(a: float, b: float) -> float:
+    """Takes float a and b as arguments and returns their sum as a float"""
+    return a + b

6f67e18cf115f3949620c91e551f6c41e4674ece
joelsteven <joelofelectronics@gmail.com>
2024-01-16 01:11:11 +0300

    Fix:task_3 coroutine

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python3
-"""Create a coroutine|task"""
+"""Create a coroutine"""
 import asyncio
-from typing import Coroutine
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-def task_wait_random(max_delay: int) -> Coroutine:
+def task_wait_random(max_delay: int) -> asyncio.Task:
     """Returns a coroutine"""
     return asyncio.create_task(wait_random(max_delay))

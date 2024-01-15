ff11505ae0f17556a394e195c0da7bb70d56dc6a
joelsteven <joelofelectronics@gmail.com>
2024-01-16 02:04:19 +0300

    Fix:asycio correct order

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -8,4 +8,5 @@ task_wait_random = __import__('3-tasks').task_wait_random
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
     """Takes 2 int arguments, returns list all delays"""
     tasks = [task_wait_random(max_delay) for _ in range(n)]
-    return await asyncio.gather(*tasks)
+    done_tasks = asyncio.as_completed(tasks)
+    return [await task for task in done_tasks]

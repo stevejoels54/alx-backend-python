ee0f7ce2e2a91dc93eadc66063e1381b2fc98c9d
joelsteven <joelofelectronics@gmail.com>
2024-01-16 00:58:55 +0300

    Added measure_time average time per interation func

---
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""Measures total execution time for wait_n(n, max_delay)"""
+import time
+import asyncio
+wait_n = __import__('1-concurrent_coroutines').wait_n
+
+
+def measure_time(n: int, max_delay: int) -> float:
+    """Return the average time per iteration"""
+    start_time = time.time()
+    asyncio.run(wait_n(n, max_delay))
+    end_time = time.time()
+
+    total_time = end_time - start_time
+    return total_time / n

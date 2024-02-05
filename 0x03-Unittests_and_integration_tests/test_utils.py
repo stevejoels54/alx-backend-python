4e3b66f88d1d588c5a3e2bab90a4c133d3e2889f
joelsteven <joelofelectronics@gmail.com>
2024-02-05 17:57:50 +0300

    Fix:imports

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -5,6 +5,7 @@ from parameterized import parameterized
 from utils import access_nested_map
 from typing import Dict, Tuple, Union
 from unittest.mock import patch, Mock
+from utils import access_nested_map, get_json, memoize
 
 
 class TestAccessNestedMap(unittest.TestCase):
